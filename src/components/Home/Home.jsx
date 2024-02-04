60435750fcb75a52ff9ea1cab67be68cba223e14
Ajeesh <ajeeshrs79@gmail.com>
2024-02-04 12:28:28 +0530

    footer added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,10 +8,16 @@ import {
   IconButton,
   Button,
   Hidden,
+  Icon,
 } from "@mui/material";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GoogleIcon from "@mui/icons-material/Google";
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
 import "./Home.css";
 import axios from "axios";
 import { getHeaders } from "../../utils/auth";
@@ -55,12 +61,9 @@ function Home() {
 
   const fetchAllProducts = async () => {
     try {
-      const response = await axios.get(
-        `${BASE_URL}/user/get-products`,
-        {
-          headers: getHeaders(),
-        }
-      );
+      const response = await axios.get(`${BASE_URL}/user/get-products`, {
+        headers: getHeaders(),
+      });
       setProductData(response.data);
     } catch (err) {
       console.log(err);
@@ -246,7 +249,7 @@ function Home() {
                   component="img"
                   image={`http://localhost:3001/uploads/${product.image.filename}`}
                   alt="Paella dish"
-                  sx={{ height: "100px",objectFit:"cover" }}
+                  sx={{ height: "100px", objectFit: "cover" }}
                 />
                 <Link to={`/user/view-product/${product._id}`}>
                   <CardContent>
@@ -354,7 +357,7 @@ function Home() {
                   component="img"
                   image={`http://localhost:3001/uploads/${product.image.filename}`}
                   alt="Paella dish"
-                  sx={{ height: "100px",objectFit:"contain" }}
+                  sx={{ height: "100px", objectFit: "contain" }}
                 />
                 <Link to={`/user/view-product/${product._id}`}>
                   <CardContent>
@@ -365,7 +368,7 @@ function Home() {
                     >
                       {product.title}
                     </Typography>
-                    
+
                     <Typography
                       sx={{ position: "absolute", bottom: "50px" }}
                       className="typo"
@@ -455,7 +458,7 @@ function Home() {
                   component="img"
                   image={`http://localhost:3001/uploads/${product.image.filename}`}
                   alt="Paella dish"
-                  sx={{ height: "100px",objectFit:"contain" }}
+                  sx={{ height: "100px", objectFit: "contain" }}
                 />
                 <Link to={`/user/view-product/${product._id}`}>
                   <CardContent>
@@ -466,7 +469,7 @@ function Home() {
                     >
                       {product.title}
                     </Typography>
-                    
+
                     <Typography
                       sx={{ position: "absolute", bottom: "50px" }}
                       className="typo"
@@ -506,6 +509,135 @@ function Home() {
               </Card>
             ))}
           </Grid>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        className="footer"
+        md={12}
+        height={"35svh"}
+        bgcolor={"#7E30E1"}
+      >
+        <Grid
+          item
+          md={6}
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems={"flex-start"}
+          justifyContent={"space-around"}
+          pl={10}
+          pt={1}
+        >
+          <Grid>
+            <Typography
+              sx={{
+                color: "#fff",
+                fontFamily: "montserrat",
+                fontWeight: "800",
+                fontSize: "20px",
+                paddingBottom: "10px",
+              }}
+            >
+              Stay productive.
+            </Typography>
+            <Typography
+              width={"55%"}
+              sx={{ color: "#fff", fontFamily: "poppins", fontSize: "13px" }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Blanditiis temporibus molestiae obcaecati quidem quaerat ex
+            </Typography>
+          </Grid>
+          <Grid>
+            <IconButton size="medium">
+              <InstagramIcon sx={{ color: "#fff" }} />
+            </IconButton>
+            <IconButton size="medium">
+              <FacebookIcon sx={{ color: "#fff" }} />
+            </IconButton>
+            <IconButton size="medium">
+              <TwitterIcon sx={{ color: "#fff" }} />
+            </IconButton>
+            <IconButton size="medium">
+              <GoogleIcon sx={{ color: "#fff" }} />
+            </IconButton>
+          </Grid>
+        </Grid>
+        <Grid
+          md={6}
+          container
+          item
+          display={"flex"}
+          flexDirection={"row"}
+          alignItems={"space-around"}
+          justifyContent={"center"}
+          pt={3}
+        >
+          <Grid md={3} item>
+            <Typography
+              pb={1}
+              sx={{
+                color: "#fff",
+                fontFamily: "poppins",
+                fontWeight: "500",
+                fontSize: "19px",
+              }}
+            >
+              Company
+            </Typography>
+            <Typography
+              sx={{ color: "#fff", fontFamily: "montserrat", fontSize: "15px" }}
+            >
+              About
+            </Typography>
+            <Typography
+              sx={{ color: "#fff", fontFamily: "montserrat", fontSize: "15px" }}
+            >
+              Services
+            </Typography>
+            <Typography
+              sx={{ color: "#fff", fontFamily: "montserrat", fontSize: "15px" }}
+            >
+              Community
+            </Typography>
+            <Typography
+              sx={{ color: "#fff", fontFamily: "montserrat", fontSize: "15px" }}
+            >
+              Help
+            </Typography>
+          </Grid>
+          <Grid md={3} item>
+            <Typography
+              pb={1}
+              sx={{
+                color: "#fff",
+                fontFamily: "poppins",
+                fontWeight: "500",
+                fontSize: "19px",
+              }}
+            >
+              Contact us
+            </Typography>
+            <Typography
+              display={"flex"}
+              alignItems={"center"}
+              sx={{ color: "#fff", fontFamily: "montserrat", fontSize: "15px" }}
+            >
+              <PhoneIcon sx={{ paddingRight: "10px" }} /> (91)0000000000
+            </Typography>
+            <Typography
+              display={"flex"}
+              alignItems={"center"}
+              sx={{ color: "#fff", fontFamily: "montserrat", fontSize: "15px" }}
+            >
+              <EmailIcon sx={{ paddingRight: "10px" }} /> support@mail.com
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid md={12} item display={"flex"} justifyContent={"center"} pt={3}>
+          <Typography sx={{ color: "#fff", fontFamily: "montserrat" }}>
+            Copyright © 2024.All Right Reserved.
+          </Typography>
         </Grid>
       </Grid>
     </>
